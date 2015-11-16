2c0d4952456c66833e20146d53158a78d91ee89a
Paul Musso <paul.musso@orange.com>
2015-11-16 14:27:03 +0100

    Remove visio sample from comsdk

--- samples/visio/gulpfile.js
+++
@@ -1,6 +0,0 @@
-require('babel/register');
-
-var requireDir = require('require-dir');
-
-// Require all tasks in gulp/tasks, including subfolders
-requireDir('./gulp/tasks', { recurse: true });
--- samples/visio/src/js/containers/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Root } from './Root';
-export { default as App } from './App';
-export { default as LoginPage } from './LoginPage';
-export { default as ParticipantPage } from './ParticipantPage';
-export { default as VisioPage } from './VisioPage';
