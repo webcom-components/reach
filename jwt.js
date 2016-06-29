e1639207291e704497e10c7adefa6d82f64fe4d1
Julien Delage <julien.delage@orange.com>
2016-06-29 15:33:13 +0200

    chore(build): fix jwt script following inquirer update

--- jwt.js
+++ jwt.js
@@ -8,39 +8,36 @@ const colors = require('colors'); //eslint-disable-line
 
 console.info('Generate a JWT token for your account:\n'.bold);
 
-inquirer.prompt(
-	[
-		{type: 'string', name: 'server', message: 'Set the test platform', 'default': 'https://webcom.orange.com'},
-		{type: 'string', name: 'email', message: 'Your account email', 'default': 'webcom.ops@orange.com'},
-		{type: 'password', name: 'password', message: 'Your account password'},
-		{type: 'password', name: 'secret', message: 'The secret for the \'accounts\' namespace'}
-	],
-	answers => {
-		if(!answers.secret){
-			console.error('\n\tMissing secret. The secret is mandatory.\n'.bold.red);
-			process.exit(0);
-		}
+inquirer.prompt([
+	{type: 'string', name: 'server', message: 'Set the test platform', 'default': 'https://io.datasync.orange.com'},
+	{type: 'string', name: 'email', message: 'Your account email'},
+	{type: 'password', name: 'password', message: 'Your account password'},
+	{type: 'password', name: 'secret', message: 'The secret for the \'accounts\' namespace'}
+]).then(answers => {
+	if(!answers.secret){
+		console.error('\n\tMissing secret. The secret is mandatory.\n'.bold.red);
+		process.exit(0);
+	}
+	const ref = new Webcom(`${answers.server}/base/accounts`);
+	ref.authWithPassword(answers)
+	.then((auth) => {
+		// Create a JWT with a 1 year validity (we don't want to change it all the time)
+		const token = jwt.sign({
+			v: 0,
+			d: {
+				uid: auth.uid,
+				email: answers.email
+			},
+			iat: moment().unix(),
+			exp: moment().add(1, 'year').unix()
+		}, answers.secret);
 
-		const ref = new Webcom(`${answers.server}/base/accounts`);
-		ref.authWithPassword(answers)
-		.then((auth) => {
-			// Create a JWT with a 1 year validity (we don't want to change it all the time)
-			const token = jwt.sign({
-				v: 0,
-				d: {
-					uid: auth.uid,
-					email: answers.email
-				},
-				iat: moment().unix(),
-				exp: moment().add(1, 'year').unix()
-			}, answers.secret);
+		console.info(`\n${token}\n`.bold);
 
-			console.info(`\n${token}\n`.bold);
-
-			process.exit(0);
-		})
-		.catch((error) => {
-			console.error(`\n\t${error.message}\n`.bold.red);
-			process.exit(0);
-		});
+		process.exit(0);
+	})
+	.catch((error) => {
+		console.error(`\n\t${error.message}\n`.bold.red);
+		process.exit(0);
 	});
+});
