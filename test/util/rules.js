cd8e611f709f5c4c6f331c71b7718e79b2913310
Julien Delage <julien.delage@orange.com>
2016-09-14 15:11:50 +0200

    chore(tests): try using fetch instead of XHR for PUT when possible

--- test/util/rules.js
+++ test/util/rules.js
@@ -1,3 +1,4 @@
+/*global fetch*/
 import * as account from './account';
 import * as config from './config';
 import * as log from './logger';
@@ -31,7 +32,40 @@ export const set = rules => {
 			return account.admin(auth.token, ns);
 		})
 		.then(token => {
-			log.w(`rules#set#admin ${token}`);
+			log.w(`rules#set#admin ${config.url}/base/${ns}/.settings/rules.json?auth=${token}`);
+			try {
+				JSON.stringify({rules});
+			} catch (e) {
+				log.w(`rules#set#json ${e.message}`);
+			}
+
+			if(global.fetch) {
+				return fetch(`${config.url}/base/${ns}/.settings/rules.json?auth=${token}`,
+					{
+						method: 'PUT',
+						headers: {
+							'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+						},
+						body: JSON.stringify({rules})
+					})
+					.then(response => {
+						log.w(`rules#set ${response.status}|${response.statusText}`);
+						if (response.status >= 200 && response.status < 300) {
+							return response.text();
+						}
+						const error = new Error(response.statusText);
+						error.response = response;
+						throw error;
+					})
+					.then(response => {
+						log.w(`rules#set ${response}`);
+					})
+					.catch(e => {
+						e.response.text().then(r => {log.w(`rules#set#fail ${r}`);});
+						return Promise.reject(e);
+					});
+			}
+
 			return new Promise((resolve, reject) => {
 				const req = new XMLHttpRequest();
 				req.onreadystatechange = function () {
