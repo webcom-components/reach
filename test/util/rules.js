e2e113759bc1b8c9c1b6bab4ab50209e722b5f9f
Julien Delage <julien.delage@orange.com>
2016-09-14 14:34:21 +0200

    chore(tests): add log in rules

--- test/util/rules.js
+++ test/util/rules.js
@@ -1,5 +1,6 @@
 import * as account from './account';
 import * as config from './config';
+import * as log from './logger';
 
 export const get = () => new Promise((resolve, reject) => {
 	const req = new XMLHttpRequest();
@@ -25,20 +26,32 @@ export const get = () => new Promise((resolve, reject) => {
 export const set = rules => {
 	const ns = global.env.namespace;
 	return account.login()
-		.then(auth => account.admin(auth.token, ns))
-		.then(token => new Promise((resolve, reject) => {
-			const req = new XMLHttpRequest();
-			req.onreadystatechange = function () {
-				if (req.readyState === 4) {
-					if (req.status === 200) {
-						resolve();
-					} else {
-						reject();
+		.then(auth => {
+			log.w('rules#set#logged');
+			return account.admin(auth.token, ns);
+		})
+		.then(token => {
+			log.w(`rules#set#admin ${token}`);
+			return new Promise((resolve, reject) => {
+				const req = new XMLHttpRequest();
+				req.onreadystatechange = function () {
+					if (req.readyState === 4) {
+						if (req.status === 200) {
+							log.w('rules#set#success');
+							resolve();
+						} else {
+							log.w('rules#set#fail');
+							reject({
+								status: req.status,
+								statusText: req.statusText
+							});
+						}
 					}
-				}
-			};
-			req.open('PUT', `${config.url}/base/${ns}/.settings/rules.json?auth=${token}`);
-			req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-			req.send(JSON.stringify({rules}));
-		}));
+				};
+				log.w(`rules#set#put '${config.url}/base/${ns}/.settings/rules.json?auth=${token}'`);
+				req.open('PUT', `${config.url}/base/${ns}/.settings/rules.json?auth=${token}`);
+				req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+				req.send(JSON.stringify({rules}));
+			});
+		});
 };
