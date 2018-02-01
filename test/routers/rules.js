55752f396c4bdbab65aa8a2b3eab5868fd5d7a8c
Le Poullennec <laurent.lepoullennec@orange.com>
2018-02-01 10:42:00 +0100

    fix(reach): check user authenticated before getting rooms and users list

--- test/routers/rules.js
+++ test/routers/rules.js
@@ -15,14 +15,17 @@ router.param('token', (req, res, next, token) => {
 	next();
 });
 
+var proxyUrl = "http://proxy:8080";
+var proxiedRequest = request.defaults({'proxy': proxyUrl});
+
 router.all('/set/:namespace/:token', (req, res) => {
-	console.log('SET', req.ns);
+	console.log('SET', req.ns, `${host}/base/${req.ns}/.settings/rules.json?auth=${req.token}`);
 	fs.createReadStream(`${__dirname}/../../dist/rules.json`)
-		.pipe(request.put({
+		.pipe(proxiedRequest.put({
 			url: `${host}/base/${req.ns}/.settings/rules.json?auth=${req.token}`,
 			headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
 		}, (error, response, body) => {
-			console.log('SET', response.statusCode, body);
+			console.log('SET', response, body, error);
 			res
 				.type(response.headers['content-type'])
 				.status(response.statusCode)
@@ -32,7 +35,7 @@ router.all('/set/:namespace/:token', (req, res) => {
 
 router.all('/reset/:namespace/:token', (req, res) => {
 	console.log('RESET', req.ns);
-	request.put({
+	proxiedRequest.put({
 		url: `${host}/base/${req.ns}/.settings/rules.json?auth=${req.token}`,
 		headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
 		json: {rules: {'.write': true, '.read': true}}
